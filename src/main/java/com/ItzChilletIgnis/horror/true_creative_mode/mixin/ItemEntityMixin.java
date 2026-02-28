232e71f450c0ed2d5981235109e0a5c5c9c07032
Steven <3347125271@qq.com>
2026-02-28 16:35:00 +0800

    1

--- src/main/java/com/ItzChilletIgnis/horror/true_creative_mode/mixin/ItemEntityMixin.java
+++ src/main/java/com/ItzChilletIgnis/horror/true_creative_mode/mixin/ItemEntityMixin.java
@@ -12,6 +12,7 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -21,26 +22,30 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class ItemEntityMixin extends Entity {
     @Shadow public abstract ItemStack getStack();
 
+    @Unique
+    private boolean isRecorded = false;
+
     public ItemEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
 
     @Inject(method = "damage", at = @At("HEAD"))
     private void onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        if (!this.getWorld().isClient && (source.isOf(DamageTypes.LAVA) || source.isOf(DamageTypes.IN_FIRE) || source.isOf(DamageTypes.CACTUS))) {
+        if (!this.getWorld().isClient && !this.isRecorded && (source.isOf(DamageTypes.LAVA) || source.isOf(DamageTypes.IN_FIRE) || source.isOf(DamageTypes.CACTUS))) {
             ItemStack stack = this.getStack();
             if (stack.getItem() instanceof ToolItem && stack.getDamage() < stack.getMaxDamage()) {
                 AbandonedToolState state = AbandonedToolState.getServerState((ServerWorld) this.getWorld());
                 state.abandonedTools.add(new AbandonedToolState.AbandonedTool(stack.copy(), null, -1, this.getWorld().getTime(), false, null));
                 state.totalAbandonedCount++;
                 state.markDirty();
+                this.isRecorded = true;
             }
         }
     }
 
     @Inject(method = "tick", at = @At("HEAD"))
     private void onTick(CallbackInfo ci) {
-        if (!this.getWorld().isClient) {
+        if (!this.getWorld().isClient && !this.isRecorded) {
             ItemEntity entity = (ItemEntity) (Object) this;
             if (entity.getAge() >= 5990) { // 接近 5 分钟
                 ItemStack stack = this.getStack();
@@ -49,6 +54,7 @@ public abstract class ItemEntityMixin extends Entity {
                     state.abandonedTools.add(new AbandonedToolState.AbandonedTool(stack.copy(), null, -1, this.getWorld().getTime(), false, null));
                     state.totalAbandonedCount++;
                     state.markDirty();
+                    this.isRecorded = true;
                 }
             }
         }
